ac1dca0c6f636fc5fd9e13093e31dbf6c7aca639
saiumesh535 <d.sai535@gmail.com>
2018-10-29 17:15:46 +0530

    finally 💖💖💖

--- utils/errorHandler.go
+++ utils/errorHandler.go
@@ -2,9 +2,25 @@ package utils
 
 import (
 	"net/http"
+
+	renderPkg "github.com/unrolled/render"
 )
 
+type ErrorMessage struct {
+	status  bool
+	message string
+}
+
+var render *renderPkg.Render
+
+func init() {
+	render = renderPkg.New() // pass options if you want
+}
+
 func Errorhandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte("Hey!! looks like dead end"))
+	response := map[string]interface{}{
+		"status":  false,
+		"message": "Doomed!!",
+	}
+	render.JSON(w, 404, response)
 }
