821b72e1d108b5f6f90129f413f4f4b2f0510cca
saiumesh535 <d.sai535@gmail.com>
2018-12-31 17:59:17 +0530

    Getting all users

--- private/users.go
+++ private/users.go
@@ -3,9 +3,28 @@ package private
 import (
 	"fmt"
 	"net/http"
+
+	model "../models"
+	renderPkg "github.com/unrolled/render"
 )
+
+type UsersData struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func init() {
+	render = renderPkg.New()
+}
 
 // GetUsers ->  getting All Users
 func GetUsers(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(fmt.Sprintf("Article %s has been created", r.Header.Get("userId"))))
+	var users []UsersData
+	err := model.FindAll(nil, &users)
+	if err != nil {
+		fmt.Println("err", err)
+		w.Write([]byte("Something wen't wrong!!"))
+	} else {
+		render.JSON(w, 200, &users)
+	}
 }
